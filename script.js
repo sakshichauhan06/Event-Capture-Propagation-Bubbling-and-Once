6b300fc9ac19686d5317ffdc8f19080eaa07d9b1
Sakshi Chauhan <chauhansakshi4m@gmail.com>
2022-02-09 18:44:51 +0530

    js changes

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const divs = document.querySelectorAll('div');
 
 function logText(e) {
     console.log(this.classList.value);
+    e.stopProagation();
 }
 
-document.body.addEventListener('click', logText);
-
-divs.forEach(div => div.addEventListener('click', logText));
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false
+}));
